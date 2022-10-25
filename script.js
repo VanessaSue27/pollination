5915f49b98c3872c5699a08e6fdf8942508c1c45
yasmingsdm <yasmin.dm@gmail.com>
2022-10-25 15:03:12 +0200

    working on js

--- script.js
+++ script.js
@@ -21,11 +21,10 @@ const pictures=[
 '↪️'
 ]
 
-// Function to choose randonly 3 pictures for each orange rectangles: (THIS IS NOT WORKING)
-
+// Function to choose randonly 3 pictures for each orange rectangles: 
 const choosePic = pictures =>{
-    return pictures [Math.floor(Math.random()*pictures.lenght)];
-    pictures.splice(pictures, 1)
+    return pictures [Math.floor(Math.random()*pictures.length)];
+    pictures.splice(pictures, 1) //NOT WORKING
 }
 
 const intro1 = choosePic(pictures)
@@ -59,17 +58,43 @@ const conc=[
 ]
 
 // Functions to appear the image when clicking on the orange rectangle:
+let mode = ''
 
-document.getElementById('introduction').onclick = ()=> {
+switch (mode){ //MAYBE MY SWITCH IS WRONG BECAUSE IT IS NOT WORKING WITH IT
+    case (document.getElementById('easy').checked ==true):
+    mode = 'easy'
+    break;
+
+    case (document.getElementById('medium').checked ==true):
+    mode = 'medium'
+    break;
+
+    case (document.getElementById('hard').checked ==true):
+    mode = 'hard'
+    break;
+
+}
+
+console.log(mode)
+
+if (mode ==='hard'){
+    document.getElementById('introduction').onclick = ()=> {
     document.getElementById('introImg').innerHTML = intro
-}
+    }
 
-document.getElementById('development').onclick = ()=> {
+    document.getElementById('development').onclick = ()=> {
     document.getElementById('developImg').innerHTML = develop
-}
+    }
 
-document.getElementById('conclusion').onclick = ()=> {
+    document.getElementById('conclusion').onclick = ()=> {
     document.getElementById('concImg').innerHTML = conc
+    }
+} else {
+    document.getElementById('play').onclick = ()=> {
+    document.getElementById('introImg').innerHTML = intro
+    document.getElementById('developImg').innerHTML = develop
+    document.getElementById('concImg').innerHTML = conc
+        }
 }
 
 //Refresh the page to play again:
